a8fc89b77630771da5a5be288031ffe9ecb0b21b
IgnacioL24 <ignaciolance24@gmail.com>
2025-02-23 18:18:38 -0300

    proxy dinamico

--- index.js
+++ index.js
@@ -3,29 +3,20 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-// Proxy para el endpoint raíz
-app.use('/api/root', createProxyMiddleware({
+// Proxy dinámico para cualquier ruta que comience con /api
+app.use('/api', createProxyMiddleware({
     target: 'http://vps-4708087-x.dattaweb.com:443',
     changeOrigin: true,
     secure: false,
-    pathRewrite: {
-        '^/api/root': ''
-    }
-}));
-
-// Proxy para el endpoint de Apollo Federation
-app.use('/api/apollo', createProxyMiddleware({
-    target: 'http://vps-4708087-x.dattaweb.com:443/apollo-federation',
-    changeOrigin: true,
-    secure: false,
-    pathRewrite: {
-        '^/api/apollo': ''
+    pathRewrite: (path, req) => {
+        // Reemplaza /api con la ruta real en el backend
+        return path.replace('/api', '');
     }
 }));
 
 // Inicializa el servidor en el puerto 3000
 app.listen(3000, () => {
-    console.log('Proxy escuchando en http://localhost:3000');
+    console.log('Proxy dinámico escuchando en http://localhost:3000');
 });
 
 module.exports = app;
