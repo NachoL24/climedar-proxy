e950adc03baf888655a86036d75adabf7c4b4fd3
IgnacioL24 <ignaciolances24@gmail.com>
2025-02-24 18:51:38 -0300

    cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Configuración CORS
 const corsOptions = {
-  origin: ['https://climedar-front.vercel.app/'],
+  origin: 'https://climedar-front.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
