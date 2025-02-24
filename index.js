ee8d8941de5f4c78c7562338a5116cfe69e7abc3
IgnacioL24 <ignaciolances24@gmail.com>
2025-02-23 21:35:57 -0300

    logs

--- index.js
+++ index.js
@@ -30,8 +30,16 @@ app.use('/', createProxyMiddleware({
   changeOrigin: true,
   secure: false,
   onProxyReq: (proxyReq, req, res) => {
+    console.log('Proxy dinámico con JWT');
+    // Loguear el método HTTP
+    console.log('Método HTTP:', req.method);
+    console.log('Body:', req.body);
+    // Loguear la URL solicitada por el cliente
+    console.log('URL solicitada por el cliente:', req.url);
     // Pasar el header Authorization con el JWT
+    console.log('Headers recibidos del cliente:', req.headers);
     const token = req.headers['authorization'] || req.headers['Authorization'];
+    console.log('JWT recibido del cliente:', token);
     if (token) {
       proxyReq.setHeader('Authorization', token);
       console.log('JWT enviado al servidor');
